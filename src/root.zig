db1409697fc1e6ae04b87e222835e21f16c3ab98
Chad DeRosier <chad.derosier@tutanota.com>
2025-03-31 10:59:30 -0700

    Revert "started meshing"
    This reverts commit 72c063ac10d546b4950942f3b6657cf6f6309fb0.

--- src/root.zig
+++ src/root.zig
@@ -8,20 +8,17 @@ const geometry = @import("geometry.zig");
 const Point = geometry.Point;
 const Edge = geometry.Edge;
 const Element2D = geometry.Element2D;
-const Array = geometry.Array;
 
 const Circle = @import("round.zig").Circle;
-
-const utils = @import("utils.zig");
 
 //pub fn run(file_path: []u8) void {
 pub fn run() void {
     const allocator = std.testing.allocator;
 
-    var points = std.AutoHashMap(Point, [6]?Array).init(allocator);
+    var points = std.AutoHashMap(Point, [2]?Edge).init(allocator);
     defer points.deinit();
 
-    var edges = std.AutoHashMap(Edge, [2]?Array).init(allocator);
+    var edges = std.AutoHashMap(Edge, [2]?usize).init(allocator);
     defer edges.deinit();
 
     var elements = std.ArrayList(Element2D).init(allocator);
@@ -29,76 +26,9 @@ pub fn run() void {
 
     const body = Circle{ .x = 0.5, .y = 0.25, .r = 3 };
 
-    mesh(body, &points, &edges, &elements);
-}
+    const shortest_edge = body.shortestEdge();
 
-fn mesh(
-    body: anytype,
-    points: *std.AutoHashMap(Point, Array),
-    edges: *std.AutoHashMap(Edge, Array),
-    elements: *std.AutoList(Element2D),
-) void {
-    //const shortest_edge = body.shortestEdge();
-    var bi = body.iterator();
-    const first_point = bi.nextPoint();
-    points.put(first_point, Array.create());
-    var last_point = first_point;
-    for (bi.nextPoint()) |point| {
-        const third_point = point_finder(last_point, point, body);
-        const body_edge = Edge{
-            .p0 = last_point,
-            .p1 = point,
-            .is_on_body = true,
-        };
-        const edge_0 = Edge{
-            .p0 = point,
-            .p1 = third_point,
-        };
-        const edge_1 = Edge{
-            .p0 = last_point,
-            .p1 = third_point,
-        };
-        const element = Element2D{
-            .e0 = body_edge,
-            .e1 = edge_0,
-            .e2 = edge_1,
-        };
-        points.put(point, Array.create());
-        points.put(third_point, Array.create());
-        edges.put(body_edge, Array.create());
-        points.get(last_point).?.append(edges.len);
-        points.get(point).?.append(edges.len);
-        edges.put(edge_0, Array.create());
-        points.get(point).?.append(edges.len);
-        points.get(third_point).?.append(edges.len);
-        edges.put(edge_1, Array.create());
-        points.get(third_point).?.append(edges.len);
-        points.get(last_point).?.append(edges.len);
-        elements.append(element);
-        edges.get(body_edge).?.append(elements.len);
-        edges.get(edge_0).?.append(elements.len);
-        edges.get(edge_0).?.append(elements.len);
-        last_point = point;
-    }
-}
-
-fn point_finder(p0: Point, p1: Point, body: Circle) Point {
-    const midpoint = utils.midpoint(p0, p1);
-    const dist = utils.distPoints(p0, p1);
-    const slope = utils.slopeRad(p0, p1);
-    var perp_slope = slope + std.math.pi / 2;
-    var proposed_point = Point{
-        .x = midpoint.x + dist * @cos(perp_slope),
-        .y = midpoint.y + dist * @sin(perp_slope),
-    };
-    if (body.isInside(proposed_point)) {
-        perp_slope += std.math.pi;
-        proposed_point = Point{
-            .x = midpoint.x + dist * @cos(perp_slope),
-            .y = midpoint.y + dist * @sin(perp_slope),
-        };
-    }
-    return proposed_point;
+    std.debug.print("debug: {d}\n", .{shortest_edge});
 }
 
 test "run" {
